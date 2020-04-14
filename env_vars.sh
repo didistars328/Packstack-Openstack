f92b746bb7b6dc49dbe4dc26605f0e1a09995837
Denys Krytskyi <didistars328@gmail.com>
2020-04-15 01:11:44 +0300

    Added draft for splited version of Openstack

--- env_vars.sh
+++ env_vars.sh
@@ -1,13 +1,58 @@
-BOX_NAME=centos/7
-MASHINE_NAME=packstack
-NAT_IP=10.0.2.15
-INT_IP=10.1.0.10
+#################
+#### GENERAL ####
+#################
+
+
+ENV=all
+PACKAGE_LOCATION=$HOME/repos/packstack
+
+##################
+### ALL IN ONE ###
+##################
+
+# Network Settings
+################################
 INT_GW=10.1.0.1
 DNS_ADDR=8.8.8.8
 NET_CIDR=255.255.255.0
 
-# Cloud Settings
+# MASHINE SETTINGS
+################################
+BOX_NAME=centos/7
+MASHINE_NAME=packstack
+NAT_IP=10.0.2.15
+INT_IP=10.1.0.10
 
+# CLOUD SETTINGS
+################################
 CLOUD_NAME=mycloud
 OPENSTACK_USER=admin
 OPENSTACK_USER_PASSWD=dragonrising
+
+#################
+### SCATTERED ###
+#################
+
+# Network Settings
+################################
+INT_GW=10.1.0.1
+TENANT_GW=172.16.0.1
+NAT_ID=100
+TENANT_ID=200
+DNS_ADDR=8.8.8.8
+NET_CIDR=255.255.255.0
+
+# MASHINES SETTINGS
+################################
+CONTROLLER=controller1
+CONTROLLER_NAT_IP=10.0.2.15
+CONTROLLER_INT_IP=10.1.0.10
+CONTROLLER_TENANT_IP=172.16.0.10
+
+COMPUTE1=compute1
+COMPUTE1_NAT_IP=10.0.2.16
+COMPUTE1_TENANT_IP=172.16.0.100
+
+COMPUTE2=compute2
+COMPUTE2_NAT_IP=10.0.2.17
+COMPUTE2_TENANT_IP=172.16.0.20
--- run_box.sh
+++ run_box.sh
@@ -1,32 +1,61 @@
 #!/bin/bash
 
+# Start
 echo "===================================="
 echo "  ----------- Load Box -----------  "
 echo "===================================="
 
+# Source ENV
 set -a
 source env_vars.sh
 set +a
 
+# Load Box
 vagrant box list | grep $BOX_NAME
 if [[ $? != 0 ]]; then
   vagrant box add $BOX_NAME
 else
   echo "$BOX_NAME is allready loaded..."
 fi
+
+# Functions:
+group_inventory () {
+echo "int_ip: $INT_IP" > $PACKAGE_LOCATION/inventory/group_vars/all
+echo "int_gw: $INT_GW" >> $PACKAGE_LOCATION/inventory/group_vars/all
+echo "netmask: $NET_CIDR" >> $PACKAGE_LOCATION/inventory/group_vars/all
+echo "dns_addr: $DNS_ADDR" >> $PACKAGE_LOCATION/inventory/group_vars/all
+echo "nat_ip: $NAT_IP" >> $PACKAGE_LOCATION/inventory/group_vars/all
+}
+
+# Run Box
 echo "===================================="
 echo "  ----------- Run Box -----------  "
 echo "===================================="
+if [[ $ENV != split ]]
+then
+  cd all_in_one/
+  vagrant up
+  PORT_ID=$(vagrant ssh-config  | grep -E 'Port' | awk '{print$2}')
+  echo "[controller]" > $PACKAGE_LOCATION/inventory/hosts
+  echo "$MASHINE_NAME ansible_host=127.0.0.1 ansible_port=$PORT_ID ansible_user=vagrant \
+  ansible_ssh_private_key_file=$PWD/.vagrant/machines/$MASHINE_NAME/virtualbox/private_key" >> $PACKAGE_LOCATION/inventory/hosts
+  group_inventory
+else
+  cd scattered/
+  vagrant up
+  echo "[controller]" > $PACKAGE_LOCATION/inventory/hosts
+  echo -e "$CONTROLLER ansible_host=127.0.0.1 ansible_port=$PORT_ID ansible_user=vagrant \
+  ansible_ssh_private_key_file=$PWD/.vagrant/machines/$CONTROLLER/virtualbox/private_key\n" >> $PACKAGE_LOCATION/inventory/hosts
+  echo "[compute]" >> $PACKAGE_LOCATION/inventory/hosts
+  for LIST in COMPUTE1 COMPUTE2
+    do
+      PORT_ID=$(vagrant ssh-config  | grep -E 'Port' | awk '{print$2}')
+      set MASHINE_NAME=$LIST
+      echo "$MASHINE_NAME ansible_host=127.0.0.1 ansible_port=$PORT_ID ansible_user=vagrant \
+      ansible_ssh_private_key_file=$PWD/.vagrant/machines/$MASHINE_NAME/virtualbox/private_key" >> $PACKAGE_LOCATION/inventory/hosts
+    done
+  group_inventory
+fi
 
-cd all_in_one/
-vagrant up
-
-echo "[controller]" > inventory/hosts
-echo "$MASHINE_NAME ansible_host=127.0.0.1 ansible_port=2222 ansible_user=vagrant ansible_ssh_private_key_file=$PWD/.vagrant/machines/$MASHINE_NAME/virtualbox/private_key" >> inventory/hosts
-echo "int_ip: $INT_IP" > inventory/group_vars/all
-echo "int_gw: $INT_GW" >> inventory/group_vars/all
-echo "netmask: $NET_CIDR" >> inventory/group_vars/all
-echo "dns_addr: $DNS_ADDR" >> inventory/group_vars/all
-echo "nat_ip: $NAT_IP" >> inventory/group_vars/all
-
-ansible-playbook -i inventory install_playbook.yml
+# Run Playbook
+cd .. && ansible-playbook -i inventory install_${ENV}.yml
