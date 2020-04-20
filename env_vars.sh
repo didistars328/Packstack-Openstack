98b689d68394c6d310edb782398fb064293516ca
Denys Krytskyi <didistars328@gmail.com>
2020-04-20 23:37:45 +0300

    Shrink scattered installer

--- env_vars.sh
+++ env_vars.sh
@@ -2,9 +2,15 @@
 #### GENERAL ####
 #################
 
+# Environment Settings
+################################
+ENV=split
+# Here is the only "all" or
+# "splited" options could be
 
-ENV=all
 PACKAGE_LOCATION=$HOME/repos/packstack
+# Here is the storage location
+# where this repo was loaded
 
 ##################
 ### ALL IN ONE ###
@@ -35,24 +41,27 @@ OPENSTACK_USER_PASSWD=dragonrising
 
 # Network Settings
 ################################
-INT_GW=10.1.0.1
+INT_GW=192.168.0.1
 TENANT_GW=172.16.0.1
-NAT_ID=100
-TENANT_ID=200
+#NAT_ID=100
+#TENANT_ID=200
 DNS_ADDR=8.8.8.8
 NET_CIDR=255.255.255.0
+FLOAT_RANGE=10.1.0.128/25
 
 # MASHINES SETTINGS
 ################################
 CONTROLLER=controller1
 CONTROLLER_NAT_IP=10.0.2.15
-CONTROLLER_INT_IP=10.1.0.10
+CONTROLLER_INT_IP=192.168.0.10
 CONTROLLER_TENANT_IP=172.16.0.10
 
 COMPUTE1=compute1
 COMPUTE1_NAT_IP=10.0.2.16
+COMPUTE1_INT_IP=192.168.0.11
 COMPUTE1_TENANT_IP=172.16.0.100
 
 COMPUTE2=compute2
 COMPUTE2_NAT_IP=10.0.2.17
+COMPUTE2_INT_IP=192.168.0.12
 COMPUTE2_TENANT_IP=172.16.0.20
--- run_box.sh
+++ run_box.sh
@@ -20,11 +20,14 @@ fi
 
 # Functions:
 group_inventory () {
+echo "env: $ENV" > $PACKAGE_LOCATION/inventory/group_vars/all
 echo "int_ip: $INT_IP" > $PACKAGE_LOCATION/inventory/group_vars/all
 echo "int_gw: $INT_GW" >> $PACKAGE_LOCATION/inventory/group_vars/all
 echo "netmask: $NET_CIDR" >> $PACKAGE_LOCATION/inventory/group_vars/all
 echo "dns_addr: $DNS_ADDR" >> $PACKAGE_LOCATION/inventory/group_vars/all
 echo "nat_ip: $NAT_IP" >> $PACKAGE_LOCATION/inventory/group_vars/all
+echo "float_range: $FLOAT_RANGE" > $PACKAGE_LOCATION/inventory/group_vars/all
+
 }
 
 # Run Box
@@ -35,25 +38,23 @@ if [[ $ENV != split ]]
 then
   cd all_in_one/
   vagrant up
-  PORT_ID=$(vagrant ssh-config  | grep -E 'Port' | awk '{print$2}')
+  PORT_ID=$(vagrant ssh-config $MASHINE_NAME | grep 'Port' | awk '{print$2}')
   echo "[controller]" > $PACKAGE_LOCATION/inventory/hosts
-  echo "$MASHINE_NAME ansible_host=127.0.0.1 ansible_port=$PORT_ID ansible_user=vagrant \
-  ansible_ssh_private_key_file=$PWD/.vagrant/machines/$MASHINE_NAME/virtualbox/private_key" >> $PACKAGE_LOCATION/inventory/hosts
+  echo "$MASHINE_NAME ansible_host=127.0.0.1 ansible_port=$PORT_ID ansible_user=vagrant ansible_ssh_private_key_file=$PWD/.vagrant/machines/$MASHINE_NAME/virtualbox/private_key" >> $PACKAGE_LOCATION/inventory/hosts
   group_inventory
 else
   cd scattered/
   vagrant up
+  PORT_ID=$(vagrant ssh-config $CONTROLLER | grep 'Port' | awk '{print$2}')
   echo "[controller]" > $PACKAGE_LOCATION/inventory/hosts
-  echo -e "$CONTROLLER ansible_host=127.0.0.1 ansible_port=$PORT_ID ansible_user=vagrant \
-  ansible_ssh_private_key_file=$PWD/.vagrant/machines/$CONTROLLER/virtualbox/private_key\n" >> $PACKAGE_LOCATION/inventory/hosts
+  echo "$CONTROLLER ansible_host=127.0.0.1 ansible_port=$PORT_ID ansible_user=vagrant ansible_ssh_private_key_file=$PWD/.vagrant/machines/$CONTROLLER/virtualbox/private_key" >> $PACKAGE_LOCATION/inventory/hosts
+  echo "" >> $PACKAGE_LOCATION/inventory/hosts
   echo "[compute]" >> $PACKAGE_LOCATION/inventory/hosts
-  for LIST in COMPUTE1 COMPUTE2
-    do
-      PORT_ID=$(vagrant ssh-config  | grep -E 'Port' | awk '{print$2}')
-      set MASHINE_NAME=$LIST
-      echo "$MASHINE_NAME ansible_host=127.0.0.1 ansible_port=$PORT_ID ansible_user=vagrant \
-      ansible_ssh_private_key_file=$PWD/.vagrant/machines/$MASHINE_NAME/virtualbox/private_key" >> $PACKAGE_LOCATION/inventory/hosts
-    done
+  for LIST in $COMPUTE1 $COMPUTE2
+  do
+    PORT_ID=$(vagrant ssh-config $LIST | grep 'Port' | awk '{print$2}')
+    echo "$LIST ansible_host=127.0.0.1 ansible_port=$PORT_ID ansible_user=vagrant ansible_ssh_private_key_file=$PWD/.vagrant/machines/$LIST/virtualbox/private_key" >> $PACKAGE_LOCATION/inventory/hosts
+  done
   group_inventory
 fi
 
